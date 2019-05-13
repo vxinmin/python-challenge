361901bde29a0f8f6e7ecb087635a65fac316bad
vxinmin <49591847+vxinmin@users.noreply.github.com>
2019-05-13 16:55:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,26 @@ with open(pybank_csv_path, newline="") as csvfile:
     # Read each row of data after the header
     for row in csvreader:
         print(row)
+        month = 0 
+        next_month = 0
+        profit_losses_list = []
+        date_list = []
+
+        month = month + 1
+        net_amount += int(row[1])
+        profit_losses_list.append(int(row[1]))
+        date_list.append(row[0])
+        average_change = (profit_losses_list[-1] - profit_losses_list[0])/(month)
+        maximum = max(profit_losses_list)
+        minimum = min(profit_losses_list)
+        
+        max_reference = profit_losses_list.index(maximum)
+        min_reference = profit_losses_list.index(minimum)
+        
+print ("Financial Analysis")
+print ("----------------------------------------")
+print (f"Total Months: {month}")
+print (f"Total : {net_amount}")
+print (f"Average Change: {average_change}")
+print (f'Greaster Increase In Profits: {date_list[max_reference]} (${maximum})')
+print (f'Greaster Decrease In Profits: {date_list[min_reference]} (${minimum})')
